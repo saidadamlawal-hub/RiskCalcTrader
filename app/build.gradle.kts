6687b286ec14511d49f3995d3f635ea36a41daa4
saidadamlawal-hub <saidadamlawal@gmail.com>
2026-08-12 23:41:56 +0100

    Final build config fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,9 +15,3 @@ android {
         versionName = "1.0"
     }
 }
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-}
